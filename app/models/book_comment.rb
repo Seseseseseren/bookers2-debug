83a5e82095e2e4ee3f18c21dd2f9e3ea98db7843
EC2 Default User <ec2-user@ip-172-31-86-207.ec2.internal>
2021-11-23 13:15:10 +0000

    [Add]Relationship

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,4 +1,5 @@
 class BookComment < ApplicationRecord
+  before_action :authenticate_user!
 
   belongs_to :user
   belongs_to :book
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, foreign_key: :followed_id
+  has_many :followings, through: :relationships, source: :follower 
+  
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :followed
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
