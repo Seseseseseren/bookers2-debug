af9b0d7b43c70823cd968bc35aba78dba7cb33e1
EC2 Default User <ec2-user@ip-172-31-86-207.ec2.internal>
2021-11-23 07:28:41 +0000

    [Add]Comment

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,11 +5,13 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @new_book = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
+    @book_comment = BookComment.new
   end
 
   def create
