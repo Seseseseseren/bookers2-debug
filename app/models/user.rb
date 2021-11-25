82e29aaedeba4aee786cefcef8aab591ca57ccc3
EC2 Default User <ec2-user@ip-172-31-86-207.ec2.internal>
2021-11-25 02:36:57 +0000

    [fix]relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :relationships, foreign_key: :followed_id
+  has_many :relationships, foreign_key: :followed_id,dependent: :destroy
   has_many :followed, through: :relationships, source: :follower 
   
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id, dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :followed
   
   def is_followed_by?(user)
